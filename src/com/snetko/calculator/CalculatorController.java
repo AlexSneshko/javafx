493c2e135f7a5c5cd044339b176e7497083bcc5d
Alex Snetko <007alex03@gmail.com>
2020-01-18 18:22:42 +0300

    done calculator

---
+++ src/com/snetko/calculator/CalculatorController.java
@@ -0,0 +1,10 @@
+package com.snetko.calculator;
+
+import javafx.scene.input.MouseEvent;
+
+public class CalculatorController {
+    public void onMouseClicked(MouseEvent event) {
+        System.out.println(event.getSource().);
+
+    }
+}
