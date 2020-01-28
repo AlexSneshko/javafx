8845f7e39fcb94c8e049258ed4593f4c9d585a1c
Alex Snetko <007alex03@gmail.com>
2020-01-28 19:19:23 +0300

    done 9tasks

--- src/com/snetko/calculator/CalculatorController.java
+++ src/com/snetko/calculator/CalculatorController.java
@@ -1,10 +1,36 @@
 package com.snetko.calculator;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 public class CalculatorController {
-    public void onMouseClicked(MouseEvent event) {
-        System.out.println(event.getSource().);
+    @FXML
+    public TextField text;
 
+
+    public void onMouseClicked(MouseEvent event) {
+        if (event.getSource() instanceof Button) {
+            Button button = (Button) event.getSource();
+            System.out.println("Pressed button: " + button.getText());
+            text.setText(text.getText() + button.getText());
+            if (button.getText().equals("CE")) {
+                text.clear();
+            }
+            if (button.getText().equals("=")) {
+
+            }
+        }
+    }
+
+    public void onKeyPressed(KeyEvent event) {
+        System.out.println(event.getText());
+        if (event.getText().equals("1") || event.getText().equals("2") || event.getText().equals("3") || event.getText().equals("4") || event.getText().equals("5") ||
+                event.getText().equals("6") || event.getText().equals("7") || event.getText().equals("9") || event.getText().equals("+") || event.getText().equals("-") ||
+                event.getText().equals("*") || event.getText().equals("/") || event.getText().equals("0")) {
+            text.setText(text.getText() + event.getText());
+        }
     }
 }
